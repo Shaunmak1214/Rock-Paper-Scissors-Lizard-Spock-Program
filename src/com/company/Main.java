a433cd066a8e7ecf8dc6f6bcd984a7819d3301ba
Rey <reykok321@gmail.com>
2021-02-12 01:07:42 +0800

    rey 2/3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main{
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         SecondFrame frame2 = new SecondFrame();
-        GameFrame gameFrame = new GameFrame();
+        //GameFrame gameFrame = new GameFrame();
 
         frame.cb.addActionListener(frame::actionPerformed);
 
@@ -28,34 +28,66 @@ public class Main{
                 switch (colorSelected)
                 {
                     case "Red":
-                        frame2.secondpanel.setBackground(Color.gray);
-                        frame2.thirdpanel.setBackground(Color.blue);
+                        frame2.secondpanel.setBackground(Color.RED);
+                        frame2.thirdpanel.setBackground(Color.RED);
+                        frame2.fourthpanel.setBackground(Color.RED);
+                        frame2.fifthpanel.setBackground(Color.RED);
+                        frame2.team1btn.setBackground(Color.RED);
+                        frame2.team2btn.setBackground(Color.RED);
+                        frame2.display1.setBackground(Color.RED);
+                        frame2.display2.setBackground(Color.RED);
                         break;
 
                     case "Green":
                         frame2.secondpanel.setBackground(Color.GREEN);
                         frame2.thirdpanel.setBackground(Color.GREEN);
+                        frame2.fourthpanel.setBackground(Color.GREEN);
+                        frame2.fifthpanel.setBackground(Color.GREEN);
+                        frame2.team1btn.setBackground(Color.GREEN);
+                        frame2.team2btn.setBackground(Color.GREEN);
+                        frame2.display1.setBackground(Color.GREEN);
+                        frame2.display2.setBackground(Color.GREEN);
                         break;
 
                     case "Blue":
                         frame2.secondpanel.setBackground(Color.BLUE);
                         frame2.thirdpanel.setBackground(Color.BLUE);
+                        frame2.fourthpanel.setBackground(Color.BLUE);
+                        frame2.fifthpanel.setBackground(Color.BLUE);
+                        frame2.team1btn.setBackground(Color.BLUE);
+                        frame2.team2btn.setBackground(Color.BLUE);
+                        frame2.display1.setBackground(Color.BLUE);
+                        frame2.display2.setBackground(Color.BLUE);
                         break;
 
                     case "Yellow":
                         frame2.secondpanel.setBackground(Color.YELLOW);
                         frame2.thirdpanel.setBackground(Color.YELLOW);
+                        frame2.fourthpanel.setBackground(Color.YELLOW);
+                        frame2.fifthpanel.setBackground(Color.YELLOW);
+                        frame2.team1btn.setBackground(Color.YELLOW);
+                        frame2.team2btn.setBackground(Color.YELLOW);
+                        frame2.display1.setBackground(Color.YELLOW);
+                        frame2.display2.setBackground(Color.YELLOW);
                         break;
 
                     case "Grey":
                         frame2.secondpanel.setBackground(Color.GRAY);
                         frame2.thirdpanel.setBackground(Color.GRAY);
+                        frame2.fourthpanel.setBackground(Color.GRAY);
+                        frame2.fifthpanel.setBackground(Color.GRAY);
+                        frame2.team1btn.setBackground(Color.GRAY);
+                        frame2.team2btn.setBackground(Color.GRAY);
+                        frame2.display1.setBackground(Color.GRAY);
+                        frame2.display2.setBackground(Color.GRAY);
                         break;
                 }
                 frame2.f2.setVisible(true);//frame 2 visible true
-                gameFrame.f3.setVisible(true);
-                gameFrame.f3.setSize(1000,700);
-                gameFrame.f3.setResizable(false);
+
+
+                //gameFrame.f3.setVisible(true);
+                //gameFrame.f3.setSize(1000,700);
+                //gameFrame.f3.setResizable(false);
 
             }
         });
--- src/com/company/SecondFrame.java
+++ src/com/company/SecondFrame.java
@@ -1,15 +1,26 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class SecondFrame {
 
     JFrame f2 = new JFrame();
 
-    JPanel firstpanel = new JPanel();
-    JPanel secondpanel = new JPanel();
-    JPanel thirdpanel = new JPanel();
+    JPanel firstpanel = new JPanel();   // TITLE PANEL
+    JPanel secondpanel = new JPanel();  // ENTER PLAYER 1 NAME PANEL (TEAM 1)
+    JPanel thirdpanel = new JPanel();   // ENTER PLAYER 1 NAME PANEL (TEAM 2)
+
+    JPanel fourthpanel = new JPanel();  // ENTER PLAYER 2 NAME PANEL (TEAM 1)
+    JPanel fifthpanel = new JPanel();   // ENTER PLAYER 2 NAME PANEL (TEAM 2)
+
+    JPanel team1btn = new JPanel();     // BUTTON FOR TEAM 1
+    JPanel team2btn = new JPanel();     // BUTTON FOR TEAM 2
+
+    JPanel display1 = new JPanel();     // Display for team 1
+    JPanel display2 = new JPanel();     // Display for team 2
 
     JLabel team1 = new JLabel("Team 1");
     JLabel team2 = new JLabel("Team 2");
@@ -17,18 +28,29 @@ public class SecondFrame {
     JLabel entername1 = new JLabel("Enter player names: ");
     JLabel entername2 = new JLabel("Enter player names: ");
 
+    JLabel t1p1 = new JLabel("Player 1:");
+    JTextField team1player1 = new JTextField();// TEXTFIELD FOR PLAYER 1 (TEAM 1)
+
+    JLabel t1p2 = new JLabel("Player 2:");
+    JTextField team1player2 = new JTextField();// TEXTFIELD FOR PLAYER 2 (TEAM 1)
 
 
+    JLabel t2p1 = new JLabel("Player 1:");
+    JTextField team2player1 = new JTextField();// TEXTFIELD FOR PLAYER 1 (TEAM 2)
 
-    //JSeparator sep = new JSeparator();
+    JLabel t2p2 = new JLabel("Player 2:");
+    JTextField team2player2 = new JTextField();// TEXTFIELD FOR PLAYER 2 (TEAM 2)
 
-    JLabel sep = new JLabel("<html><br/></html>",SwingConstants.CENTER);
+    JButton button1 = new JButton("Enter");// BUTTON FOR TEAM 1
+    JButton button2 = new JButton("Enter");// BUTTON FOR TEAM 2
 
-    JLabel upperlabel = new JLabel();
+    JLabel upperlabel = new JLabel();// LABEL FOR TITLE
+
+    JLabel results1 = new JLabel();
+    JLabel results2 = new JLabel();
 
     SecondFrame()
     {
-        //s.setOrientation(SwingConstants.HORIZONTAL);
 
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
@@ -38,95 +60,169 @@ public class SecondFrame {
         firstpanel.setBorder((BorderFactory.createEmptyBorder(10,10,10,10)));//PANEL LAYOUT FOR LABEL
 
         f2.add(firstpanel,BorderLayout.NORTH);//panel position
-        firstpanel.setBackground(Color.ORANGE);//BACKGROUND COLOR OF FIRST PANEL
+        firstpanel.setBackground(new Color(253, 105, 0));//BACKGROUND COLOR OF FIRST PANEL
         firstpanel.setPreferredSize(new Dimension(1000,60));//FIRST PANEL SIZE
 
         upperlabel.setText("Rock-Paper-Scissors-Lizard-Spock");// the label text
-        upperlabel.setFont(new Font("Verdana",Font.PLAIN,18));//font of text
+        upperlabel.setFont(new Font("Sans Serif",Font.BOLD,18));//font of text
         upperlabel.setBackground(Color.ORANGE);//text background color
 
-        firstpanel.add(upperlabel);
-        upperlabel.setOpaque(true);
-
-        entername1.setFont(new Font("Verdana", Font.PLAIN,19));
-        entername2.setFont(new Font("Verdana",Font.PLAIN,19));
+        firstpanel.add(upperlabel);// ADDED LABEL TO TOP PANEL
+        //upperlabel.setOpaque(true);
 //////////////////////  IMPORTANTTTTTTTTTTTTTTTTTTTTTTT //////////////////////
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
+        entername1.setFont(new Font("Verdana", Font.PLAIN,19));// FONT FOR "Enter Player Names:" (TEAM 1)
+        entername2.setFont(new Font("Verdana",Font.PLAIN,19));// FONT FOR "Enter Player Names:" (TEAM 2)
+
+        button1.setFont(new Font("Verdana", Font.BOLD,14));// FONT FOR "ENTER" BUTTON (TEAM 1)
+        button2.setFont(new Font("Verdana", Font.BOLD,14));// FONT FOR "ENTER" BUTTON (TEAM 2)
+
+        team1.setFont(new Font("Verdana",Font.PLAIN,20));// FONT FOR "Team 1"
+        team2.setFont(new Font("Verdana",Font.PLAIN,20));// FONT FOR "Team 2"
+
 ///////////////// SECOND PANEL BELOW////////////////////////////
 
-        secondpanel.setBorder((BorderFactory.createEmptyBorder(100,0,0,0)));
-        f2.add(secondpanel,BorderLayout.WEST);
-        secondpanel.setPreferredSize(new Dimension(500,620));
+//        t1p1.setLabelFor(team1player1);
+//        t1p2.setLabelFor(team1player2);
+
+        secondpanel.setBorder((BorderFactory.createEmptyBorder(100,0,0,0)));// SECOND PANEL POSITION
+        f2.add(secondpanel,BorderLayout.WEST);// ADD SECOND PANEL TO FRAME
+        secondpanel.setPreferredSize(new Dimension(500,300));// SET SIZE FOR SECOND PANEL
+
+        secondpanel.add(team1);// ADDED "Team 1" LABEL
+        //team1.setOpaque(true);
+        secondpanel.add(entername1);// ADDED "Enter Player Name:" (TEAM 1)
+        entername1.setBorder((BorderFactory.createEmptyBorder(20,250,30,250)));// SET "Enter Player Name:" POSITION
+
+        team1player1.setPreferredSize(new Dimension(200,20));// SET TEXT FIELD SIZE
+
+        secondpanel.add(t1p1);// ADD "Player 1:" LABEL
+        secondpanel.add(team1player1);// ADD TEXT FIELD
 
 
-        //team1.setText("Team 1");
-        //team1.setBorder((BorderFactory.createEmptyBorder(100,100,20,0)));
-        //team1.setFont(new Font("Verdana",Font.PLAIN,20));
+//////////////// THIRD PANEL BELOW (TEAM 1, PLAYER 2)///////////////////////////
+        secondpanel.add(thirdpanel);// ADD THIRD PANEL FOR PLAYER 2
 
-        f2.add(team1);
-        secondpanel.add(team1);
-        team1.setOpaque(true);
-        secondpanel.add(entername1);
-        entername1.setBorder((BorderFactory.createEmptyBorder(0,250,30,250)));
+        team1player2.setPreferredSize(new Dimension(200,20));// SET SIZE FOR TEXT FIELD
+        thirdpanel.setBorder(BorderFactory.createEmptyBorder(20,250,0,250));// SET THIRD PANEL POSITION
 
-        JLabel t1p1 = new JLabel("Player 1:");
-        JTextField team1player1 = new JTextField();
+        thirdpanel.add(t1p2);// ADD "Player 2:" LABEL
+        thirdpanel.add(team1player2);// ADD TEXT FIELD
 
-        secondpanel.add(t1p1);
-        secondpanel.add(team1player1);
-        team1player1.setPreferredSize(new Dimension(200,20));
+//////////////// TEAM 1 "ENTER" BUTTON PANEL/////////////////////////////
+        secondpanel.add(team1btn);
 
-        //f2.add(sep);
-        JLabel t1p2 = new JLabel("Player 2:");
-        JTextField team1player2 = new JTextField();
-
-        secondpanel.add(t1p2);
-        secondpanel.add(team1player2);
-
-        t1p2.setBorder((BorderFactory.createEmptyBorder(100,0,0,0)));
-        team1player2.setPreferredSize(new Dimension(200,20));
-        team1player2.setBorder((BorderFactory.createEmptyBorder(0,250,0,250)));
-
-        //secondpanel.setPreferredSize(new Dimension(500,620));
-
-//////////////// THIRD PANEL BELOW///////////////////////////
-        thirdpanel.setBorder((BorderFactory.createEmptyBorder(100,0,0,0)));
-        f2.add(thirdpanel,BorderLayout.EAST);
-        thirdpanel.setPreferredSize(new Dimension(500,620));
-
-        //team2.setText("Team 2");
-        //team2.setBorder((BorderFactory.createEmptyBorder(100,1000,20,0)));
-        //team2.setFont(new Font("Verdana",Font.PLAIN,20));
-
-        entername2.setBorder((BorderFactory.createEmptyBorder(100,250,0,250)));
+        team1btn.setBorder(BorderFactory.createEmptyBorder(25,250,0,250));
+        button1.setPreferredSize(new Dimension(100,30));
+        button1.setForeground(Color.BLUE);
 
 
+        team1btn.add(button1);
 
+///////////////// DISPLAY RESULTS FOR TEAM 1
+        secondpanel.add(display1);
+
+        display1.setBorder(BorderFactory.createEmptyBorder(0,250,20,250));
+        display1.add(results1);
+//        JLabel testing = new JLabel("Testing");
+        //display1.add(testing);
+
+        button1.addActionListener(this::btn1Check);
+
+//////////////// FOURTH PANEL BELOW///////////////////////////
+        fourthpanel.setBorder((BorderFactory.createEmptyBorder(100,0,0,0)));
+        f2.add(fourthpanel,BorderLayout.EAST);
+        fourthpanel.setPreferredSize(new Dimension(500,300));
+
+        entername2.setBorder((BorderFactory.createEmptyBorder(20,250,0,250)));
 
         f2.add(team2);
-        thirdpanel.add(team2);
-        team2.setOpaque(true);
-        thirdpanel.add(entername2);
-        entername2.setBorder((BorderFactory.createEmptyBorder(0,250,30,250)));
+        fourthpanel.add(team2);
+        //team2.setOpaque(true);
+        fourthpanel.add(entername2);
+        entername2.setBorder((BorderFactory.createEmptyBorder(20,250,30,250)));
 
-        JLabel t2p1 = new JLabel("Player 1:");
-        JTextField team2player1 = new JTextField();
-
-        team2player1.setBorder((BorderFactory.createEmptyBorder()));
         team2player1.setPreferredSize(new Dimension(200,20));
 
-        thirdpanel.add(t2p1);
-        thirdpanel.add(team2player1);
+        fourthpanel.add(t2p1);
+        fourthpanel.add(team2player1);
 
-        JLabel t2p2 = new JLabel("Player 2:");
-        JTextField team2player2 = new JTextField();
 
-        thirdpanel.add(t2p2);
-        thirdpanel.add(team2player2);
+//////////////// FIFTH PANEL BELOW (TEAM 2, PLAYER 2)//////////////////////////
+        fourthpanel.add(fifthpanel);
 
-        //thirdpanel.setPreferredSize(new Dimension(500,620));
+        team2player2.setPreferredSize(new Dimension(200,20));
+        fifthpanel.setBorder(BorderFactory.createEmptyBorder(20,250,0,250));
+        fifthpanel.add(t2p2);
+        fifthpanel.add(team2player2);
+
+//////////////// TEAM 2 "ENTER" BUTTON PANEL//////////////////////////////////
+        fourthpanel.add(team2btn);
+
+        team2btn.setBorder(BorderFactory.createEmptyBorder(25,250,0,250));
+        button2.setPreferredSize(new Dimension(100,30));
+        button2.setForeground(Color.BLUE);
+
+        button2.addActionListener(this::btn2Check);
+        team2btn.add(button2);
+
+//////////////// DISPLAY RESULTS FOR TEAM 2 /////////////////////////////////
+        fourthpanel.add(display2);
+        display2.setBorder(BorderFactory.createEmptyBorder(10,250,0,250));
+
+        display2.add(results2);
+    }
+
+    public void btn1Check(ActionEvent btn1)
+    {
+        if(team1player1.getText().trim().isEmpty() && team1player2.getText().trim().isEmpty())
+        {
+            results1.setText("Please enter name for both Player");
+        }
+        else if(team1player1.getText().trim().isEmpty())
+        {
+            results1.setText("Please enter name for Player 1");
+        }
+        else if(team1player2.getText().trim().isEmpty())
+        {
+            results1.setText("Please enter name for Player 2");
+        }
+        else
+        {
+            results1.setText("Ready!");
+
+            if(results1.getText().equals("Ready!") && results2.getText().equals("Ready!"))
+            {
+
+            }
+        }
+    }
+
+    public void btn2Check(ActionEvent btn2)
+    {
+        if(team2player1.getText().trim().isEmpty() && team2player2.getText().trim().isEmpty())
+        {
+            results2.setText("Please enter name for both Player");
+        }
+        else if(team2player1.getText().trim().isEmpty())
+        {
+            results2.setText("Please enter name for Player 1");
+        }
+        else if(team2player2.getText().trim().isEmpty())
+        {
+            results2.setText("Please enter name for Player 2");
+        }
+        else
+        {
+            results2.setText("Ready!");
+
+            if(results1.getText().equals("Ready!") && results2.getText().equals("Ready!"))
+            {
+
+            }
+        }
 
 
     }
